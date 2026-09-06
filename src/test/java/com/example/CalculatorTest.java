9c95658c07e83a8d66c0962d0971b6f53db9af19
Adil Shajahan <adil@gmail.com>
2026-09-06 12:54:23 +0530

    inital calculator project

---
+++ src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(2, calculator.subtract(5, 3));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(15, calculator.multiply(5, 3));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+    }
+}
